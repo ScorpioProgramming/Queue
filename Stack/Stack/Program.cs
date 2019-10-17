3801e78b56e4d87a9a8f28ba833edbeb5ea567fe
ScorpioProgramming <46716919+ScorpioProgramming@users.noreply.github.com>
2019-10-17 16:32:00 +0200

    Initial commit

---
+++ Stack/Stack/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack stack = new Stack(10);
+            stack.Enqueue(2);
+            stack.Enqueue(13);
+            stack.Enqueue(40);
+            stack.Enqueue(27);
+            Console.WriteLine(stack.Peek());
+            stack.Dequeue();
+            Console.WriteLine(stack.Peek());
+            stack.Clear();
+        }
+    }
+}
